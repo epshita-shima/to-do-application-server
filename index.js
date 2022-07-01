b8c156c5a3c1cbb00206015f882325c57dc6a960
epshita-shima <epshitashima@gmail.com>
2022-07-01 08:31:10 +0600

    get data for todo list

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = "mongodb+srv://db_todo:214wx4pEoSa35S8i@cluster0.cnsel.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db("todoList").collection("task");
+
+        app.get('/task', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const data = await cursor.toArray()
+            res.send(data);
+        })
+        app.get('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const completeTask = await taskCollection.findOne(query);
+            res.send(completeTask);
+        })
+        app.post('/task', async (req, res) => {
+            const newTask = req.body;
+            console.log("addning new info", newTask);
+            const result = await taskCollection.insertOne(newTask);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('To Do server is running');
